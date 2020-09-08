f5a56f5aa34068ee9338bd4b9fb2485c3a189855
Gabriel Vendrame <info@gabrielvendrame.it>
2020-09-08 16:24:54 +0200

    Upload first test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test';
+  chart: Chart;
+
+  ngOnInit(): void {
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+        datasets: [{
+          label: 'Fatturato 2018',
+          data: [1000, 12000, 13000, 900, 1000, 11000, 14000, 18000, 23000, 500],
+          backgroundColor: [
+            'rgba(255, 133, 49, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 206, 86, 1)',
+          ],
+          borderWidth: 1
+        },
+          {
+            label: 'Fatturato 2019',
+            data: [12000, 1000, 30000, 5333, 9, 8],
+            backgroundColor: [
+              'rgba(255, 133, 69, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 3, 86, 1)',
+            ],
+            borderWidth: 1
+          }]
+      },
+    });
+  }
+
 }
